4dd93cf66edd3066d2b1409111ac6a9c701704be
Anand Singh <reaching.anandsingh@gmail.com>
2025-06-13 22:05:43 +0530

    Completed Day 1 challenges

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator!')
+city_name = input('Which city did you grow up in?\n')
+pet_name = input('What is the name of your pet?\n')
+print('Your band name could be: ' +
+       city_name +
+      ' ' +
+      pet_name)
