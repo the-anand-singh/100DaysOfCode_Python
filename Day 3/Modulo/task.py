f5a1a1d922fd6e627e8196eb0984c9d2261549bc
Anand Singh <reaching.anandsingh@gmail.com>
2025-06-14 16:40:05 +0530

    Completed Day 3 challenges

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,9 @@
+# Pause 1:
+print(10 % 3)
+
+# Pause 2: Odd or Even
+number = int(input('Enter a number: '))
+if number % 2:
+    print('Number is Odd!')
+else:
+    print('Number is Even!')
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,22 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+total_price = 0
+
+if size == 'S':
+    total_price += 15
+    if pepperoni == 'Y':
+        total_price += 2
+elif size == 'M':
+    total_price += 20
+    if pepperoni == 'Y':
+        total_price += 3
+elif size == 'L':
+    total_price += 25
+    if pepperoni == 'Y':
+        total_price += 3
+
+if extra_cheese == 'Y':
+    total_price += 1
+
+print(f'Your final bill is: ${total_price}.')
