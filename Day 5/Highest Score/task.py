640ee1c0fc6d2f18e15358ce74331319cd728e4a
Anand Singh <reaching.anandsingh@gmail.com>
2025-06-19 22:35:04 +0530

    Completed Day 5 challenges

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,12 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
+
+print(f'Maximum score: {max(student_scores)}')
+
+# Without MAX
+max_score = 0
+for student_score in student_scores:
+    if max_score < student_score:
+        max_score = student_score
+
+print(f'Maximum score: {max_score}')
